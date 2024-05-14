7050751c8df9ef521978f2226c3cd2d52ffcd36d
Hamid Hassani <hamidhassani119@gmail.com>
2024-05-14 12:15:57 +0330

    Smooth scroll added to the project now the the page would be scrolled by click on the learn more btn

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -29,3 +30,25 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// const message = document.createElement('div');
+
+// message.classList.add('cookie-message');
+
+// message.innerHTML =
+//   'We use cookies to improve functionality and analytics <button class="btn btn--close-cookie" >Got it!</button>';
+
+// header.prepend(message);
+
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
+
+const section1 = document.querySelector('#section--1');
+const scrollBtn = document
+  .querySelector('.btn--scroll-to')
+  .addEventListener('click', function () {
+    section1.scrollIntoView({ behavior: 'smooth' });
+  });
